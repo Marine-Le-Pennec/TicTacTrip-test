65b7da35c969326ecc618f8dabada73193ded683
OctoOcto <66621960+OctoOcto@users.noreply.github.com>
2020-10-08 10:24:19 +0200

    responsive composants

---
+++ src/Components/Responsive/ArriveChoice.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ArriveChoice = ({
+  modalDisplayArrive,
+  modalHideDepart,
+  arriveInput,
+  setArriveInput,
+}) => {
+  return (
+    <div
+      className="input-container-mobile"
+      onClick={() => {
+        modalDisplayArrive();
+        modalHideDepart();
+      }}
+    >
+      <h3>Arrivée</h3>
+      <input
+        value={arriveInput}
+        placeholder="Ville ou aéroports"
+        type="text"
+        onChange={(event) => {
+          setArriveInput(event.target.value);
+        }}
+      />
+    </div>
+  );
+};
+
+export default ArriveChoice;
